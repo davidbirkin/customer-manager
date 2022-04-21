f0939b59ffbd340796cf8098fb372bac4bbc71d5
David Birkin <davidbirkin1988@gmail.com>
2022-04-20 20:55:04 -0400

    Updated Color Theme / Added Delete button to Edit Modal

--- resources/views/livewire/roles/rolepage.blade.php
+++ resources/views/livewire/roles/rolepage.blade.php
@@ -27,12 +27,9 @@
                             <td>{{ $role->users_count }}</td>
                             <td>{{ $role->created_at->diffForHumans() }}</td>
                             <td>
-                                <button class="btn btn-danger btn-sm float-end"
-                                    wire:click.prevent="deleteConfirm({{ $role->id }})">Delete
-                                    Role</button>
-                                <button class="btn btn-warning btn-sm float-end" style="margin-right: 10px !important"
-                                    data-coreui-toggle="modal" wire:click.prevent="edit({{ $role->id }})">Edit
-                                    Role</button>
+                                <button class="btn btn-warning btn-sm float-end"
+                                    style="margin-right: 10px !important; color:white;" data-coreui-toggle="modal"
+                                    wire:click.prevent="edit({{ $role->id }})">Edit</button>
                             </td>
                         </tr>
                     @empty
@@ -71,9 +68,11 @@
                                 </div>
                             </div>
                             <div class="modal-footer border-top-0">
-                                <button type="button" class="btn btn-secondary"
-                                    data-coreui-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary"
+                                <button type="button" class="btn btn-secondary btn-sm" data-coreui-dismiss="modal"
+                                    style="color:white; background-color:  !important ">Close</button>
+                                <button class="btn btn-danger btn-sm float-end"
+                                    wire:click.prevent="deleteConfirm({{ $role->id }})">Delete</button>
+                                <button type="button" class="btn btn-sm btn-primary"
                                     wire:click.prevent="save(@if ($modal_title == 'Add Role') true @else false @endif)">{{ $modal_button_text ?? '' }}</button>
                             </div>
                         </div>
